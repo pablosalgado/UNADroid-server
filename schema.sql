31dfeb3c6f74458e8f59177c1ca4405b10578d32
Pablo Salgado <pablo.salgado@iungoeducation.com>
2018-05-20 17:48:25 -0500

    Completa esquema de base de datos.

--- schema.sql
+++ schema.sql
@@ -1,13 +1,117 @@
-DROP TABLE `users`;
+DROP TABLE IF EXISTS `resource`;
+DROP TABLE IF EXISTS `resource_type`;
+DROP TABLE IF EXISTS `user_extra_activity`;
+DROP TABLE IF EXISTS `user_evaluation`;
+DROP TABLE IF EXISTS `evaluation_answer`;
+DROP TABLE IF EXISTS `evaluation_question`;
+DROP TABLE IF EXISTS `evaluation`;
+DROP TABLE IF EXISTS `extra_activity`;
+DROP TABLE IF EXISTS `unit`;
+DROP TABLE IF EXISTS `user`;
 
-CREATE TABLE `users` (
+CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `firstName` varchar(100),
-  `lastName` varchar(100),
-  `createdAt` datetime,
-  `updatedAt` datetime,
+  `firstName` varchar(100) DEFAULT NULL,
+  `lastName` varchar(100) DEFAULT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_UN` (`email`)
-) ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `unit` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `extra_activity` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `points` int(11) NOT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `evaluation` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `unit_id` int(11) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `evaluation_question` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `evaluation_id` int(11) NOT NULL,
+  `question` varchar(100) NOT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `evaluation_question_evaluation_FK` (`evaluation_id`),
+  CONSTRAINT `evaluation_question_evaluation_FK` FOREIGN KEY (`evaluation_id`) REFERENCES `evaluation` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `evaluation_answer` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `evaluation_question_id` int(11) NOT NULL,
+  `answer` varchar(100) NOT NULL,
+  `correct` bit(1) NOT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `evaluation_answer_evaluation_question_FK` (`evaluation_question_id`),
+  CONSTRAINT `evaluation_answer_evaluation_question_FK` FOREIGN KEY (`evaluation_question_id`) REFERENCES `evaluation_question` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `user_evaluation` (
+  `user_id` int(11) NOT NULL,
+  `evaluation_id` int(11) NOT NULL,
+  `grade` int(11) NOT NULL,
+  KEY `user_evaluation_user_FK` (`user_id`),
+  KEY `user_evaluation_evaluation_FK` (`evaluation_id`),
+  CONSTRAINT `user_evaluation_evaluation_FK` FOREIGN KEY (`evaluation_id`) REFERENCES `evaluation` (`id`),
+  CONSTRAINT `user_evaluation_user_FK` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `user_extra_activity` (
+  `user_id` int(11) NOT NULL,
+  `extra_activity_id` int(11) NOT NULL,
+  KEY `user_extra_activity_user_FK` (`user_id`),
+  KEY `user_extra_activity_extra_activity_FK` (`extra_activity_id`),
+  CONSTRAINT `user_extra_activity_extra_activity_FK` FOREIGN KEY (`extra_activity_id`) REFERENCES `extra_activity` (`id`),
+  CONSTRAINT `user_extra_activity_user_FK` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `resource_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `resource` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `resource_type_id` int(11) NOT NULL,
+  `unit_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `url` varchar(100) NOT NULL,
+  `order` int(11) NOT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  `updatedAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `resource_resource_type_FK` (`resource_type_id`),
+  KEY `resource_unit_FK` (`unit_id`),
+  CONSTRAINT `resource_resource_type_FK` FOREIGN KEY (`resource_type_id`) REFERENCES `resource_type` (`id`),
+  CONSTRAINT `resource_unit_FK` FOREIGN KEY (`unit_id`) REFERENCES `unit` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
